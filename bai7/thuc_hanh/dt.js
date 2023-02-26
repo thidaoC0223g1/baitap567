a28fd450f3f4642ac8ddd2843fd9e3340586b107
thidaoC0223g1 <daogiathi231290@gmail.com>
2023-02-26 07:50:11 +0700

    bai tap bao 7

---
+++ bai7/thuc_hanh/dt.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("nhap vao chieu rong");
+inputHeight = prompt("nhap vao chieu cao");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("dien tich hinh chu nhat = " + area);
